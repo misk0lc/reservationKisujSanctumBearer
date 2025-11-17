99e3b48ff97cbd25d33b49132a6ef36c16438a15
misk0lc <gyorfi1552@gmail.com>
2025-11-17 09:25:31 +0100

    tényleg kész

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,12 @@ Route::get('/hello', function (Request $request) {
     return response()->json(['message'=>'Hello API']);
 });
 Route::post('/register', [AuthController::class, 'register']);
-
 Route::post('/login', [AuthController::class, 'login']);
 
 //Authentikált végpontok
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+//ezeket is alakítani kell
 Route::get('/reservations',[ReservationController::class, 'index']); // összes foglalás
 Route::get('/reservations/{id}',[ReservationController::class, 'show']); // egy foglalás
 Route::post('/reservations',[ReservationController::class, 'store']); // egy foglalás rögzítése
